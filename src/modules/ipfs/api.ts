504dc5fcb3fead9104c588291134124aa889348d
Chirag Mahaveer Parmar <chirag.parmar@slock.it>
2019-03-28 18:01:05 +0100

    fixes

--- src/modules/ipfs/api.ts
+++ src/modules/ipfs/api.ts
@@ -1,6 +1,4 @@
 import Client from '../../client/Client'
-const bs58 = require('bs58')
-import {verifyIPFSHash} from './ipfs'
 
 export default class API{
   client: Client
@@ -11,16 +9,14 @@ export default class API{
 
   get(hash: string, resultEncoding?: string): Promise<Buffer> {
     return this.client.sendRPC('ipfs_get', [hash, resultEncoding || 'base64'], '0x7d0').then(response => {
-      if(response.result) return Buffer.from(response.result, 'base64')
-      else Promise.reject(response.error || 'Hash not found')
+      return Buffer.from(response.result, resultEncoding || 'base64') || Promise.reject(response.error || 'Hash not found')
     })
   }
 
 
   put(data: string, dataEncoding?: string): Promise<string> {
     return this.client.sendRPC('ipfs_put', [data, dataEncoding || 'base64'], '0x7d0').then(response => {
-      if(response.result) return response.result
-      else Promise.reject(response.error || 'Hash not found')
+      return response.result || Promise.reject(response.error || 'Hash not found')
     })
   }
 }
