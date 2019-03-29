b63ec128959b578bb0dbedbe1ebd9c0d3b33f94a
Chirag Mahaveer Parmar <chirag.parmar@slock.it>
2019-03-29 12:07:03 +0100

    encoding confusion

--- src/modules/ipfs/api.ts
+++ src/modules/ipfs/api.ts
@@ -9,14 +9,16 @@ export default class API{
 
   get(hash: string, resultEncoding?: string): Promise<Buffer> {
     return this.client.sendRPC('ipfs_get', [hash, resultEncoding || 'base64'], '0x7d0').then(response => {
-      return Buffer.from(response.result, resultEncoding || 'base64') || Promise.reject(response.error || 'Hash not found')
+      if(response.result) return Buffer.from(response.result, resultEncoding || 'base64')
+      else Promise.reject(response.error || 'Hash not found')
     })
   }
 
 
-  put(data: string, dataEncoding?: string): Promise<string> {
-    return this.client.sendRPC('ipfs_put', [data, dataEncoding || 'base64'], '0x7d0').then(response => {
-      return response.result || Promise.reject(response.error || 'Hash not found')
+  put(data: Buffer, dataEncoding?: string): Promise<string> {
+    return this.client.sendRPC('ipfs_put', [data.toString(dataEncoding), dataEncoding || 'base64'], '0x7d0').then(response => {
+      if(response.result) return response.result
+      else Promise.reject(response.error || 'Hash not found')
     })
   }
 }
