3c9fe95df640a9c92233bdc9f968272db9f2e642
Simon Jentzsch <simon@slock.it>
2019-03-29 13:46:24 +0100

    fixedx ts

--- src/modules/ipfs/api.ts
+++ src/modules/ipfs/api.ts
@@ -1,22 +1,21 @@
 import Client from '../../client/Client'
 
-export default class API{
+export default class API {
   client: Client
 
-  constructor(_client: Client){
+  constructor(_client: Client) {
     this.client = _client
   }
 
   get(hash: string, resultEncoding?: string): Promise<Buffer> {
-    return this.client.sendRPC('ipfs_get', [hash, resultEncoding || 'base64'], '0x7d0').then(response =>
-      response.result? Buffer.from(response.result, resultEncoding || 'base64') : Promise.reject(response.error || 'Hash not found')
-    )
+    return this.client.sendRPC('ipfs_get', [hash, resultEncoding || 'base64'], '0x7d0')
+      .then(response => response.result ? Buffer.from(response.result, resultEncoding || 'base64') : Promise.reject(new Error(response.error || 'Hash not found')) as any)
   }
 
 
   put(data: Buffer, dataEncoding?: string): Promise<string> {
-    return this.client.sendRPC('ipfs_put', [data.toString(dataEncoding||'base64'), dataEncoding || 'base64'], '0x7d0').then(response =>
-      response.result? response.result : Promise.reject(response.error || 'Hash not found')
+    return this.client.sendRPC('ipfs_put', [data.toString(dataEncoding || 'base64'), dataEncoding || 'base64'], '0x7d0').then(response =>
+      response.result ? response.result : Promise.reject(new Error(response.error || 'Hash not found'))
     )
   }
 }
