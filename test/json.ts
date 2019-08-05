b2a992fb47e1d357aec41a6b68017ed0583bdae2
Junaid <junaid.taj@slock.it>
2019-08-05 14:47:47 +0200

    allowing only json

--- test/json.ts
+++ test/json.ts
@@ -7,9 +7,9 @@ for (const a of process.argv.slice(2)) {
     if (a == '-t') filter = -2
     else if (filter === -2)
         filter = parseInt(a)
-    else
+    else if(a.indexOf('.json') != -1)
         files.push(a)
 }
 //console.log("files:", JSON.stringify(files))
-//run_test(files, filter).then(() => console.log('done'), console.log)
+run_test(files, filter).then(() => console.log('done'), console.log)
 
