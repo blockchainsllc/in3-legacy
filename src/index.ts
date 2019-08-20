63d4912a6e3c1c4890506f45146827095b3a7395
Junaid <junaid.taj@slock.it>
2019-08-20 16:42:42 +0200

    reexporting all default exports of in3 public api

--- src/index.ts
+++ src/index.ts
@@ -22,14 +22,28 @@ export const util = _util
 
 import Client, { aliases } from './client/Client'
 import EthAPI_ from './modules/eth/api'
+import { validate as validateOb } from 'in3-common'
 export default Client
 
 export const EthAPI = EthAPI_
 
 export type IN3Client = Client
 
+import { cbor as _cbor } from 'in3-common'
+export const cbor = _cbor
+
+import * as _serialize from 'in3-common/js/src/modules/eth/serialize'
+export const serialize = _serialize
+
 import * as _header from './modules/eth/header'
 export const header = _header
+
+import { storage as _storage } from 'in3-common'
+export const storage = _storage
+
+import { transport as _transport } from 'in3-common'
+import { Transport as _transporttype } from 'in3-common'
+export const transport = _transport
 
 import * as _chainData from './modules/eth/chainData'
 export const chainData = _chainData
@@ -52,10 +66,17 @@ export type Proof = types.Proof
 export type AuraValidatoryProof = types.AuraValidatoryProof
 export type RPCResponse = types.RPCResponse
 export type Signature = types.Signature
+export type Transport = _transporttype
 export type ServerList = types.ServerList
+export type BlockData = _serialize.BlockData 
+export type LogData = _serialize.LogData 
+export type ReceiptData = _serialize.ReceiptData 
+export type TransactionData = _serialize.TransactionData 
 export type IN3RPCConfig = types.IN3RPCConfig
 export type IN3RPCHandlerConfig = types.IN3RPCHandlerConfig
 export type ChainSpec = types.ChainSpec
 
+export const AxiosTransport = transport.AxiosTransport
 export const typeDefs = types.validationDef
+export const validate = validateOb.validate 
 export const chainAliases = aliases
