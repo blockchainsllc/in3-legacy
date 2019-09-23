f58909740a5c43001ce547b2b892c361ae949bd7
M Junaid Taj <junaid.taj@slock.it>
2019-09-23 16:11:11 +0200

    in3 examples

---
+++ examples/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -rf node_modules
+npm i
+npm start
+
+# cd ../
+# npm i && npm run build
+# mkdir examples/node_modules/in3
+# cp -r `ls -A | grep -v "examples"` examples/node_modules/in3/
